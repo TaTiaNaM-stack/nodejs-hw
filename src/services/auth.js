4704f0292fff7e98eba2b4de4db5f5a9d50e4d48
TaTiaNaM <maluchenko.tania@gmail.com>
2026-05-21 16:36:36 +0300

    random

--- src/services/auth.js
+++ src/services/auth.js
@@ -5,13 +5,13 @@ import { Session } from '../models/session.js';
 export const createSession = async (userId) => {
   const accessToken = crypto.randomUUID();
   const refreshToken = crypto.randomUUID();
-  const sessionId = crypto.randomUUID();
+
 
   return Session.create({
     userId,
     accessToken,
     refreshToken,
-    _id: sessionId,
+    _id: crypto.randomUUID(),
     accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
     refreshTokenValidUntil: new Date(Date.now() + ONE_DAY),
   });
