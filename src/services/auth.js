f4f8093e416aa202f7bf261558b244d8ecd8c7f6
TaTiaNaM <maluchenko.tania@gmail.com>
2026-05-21 16:30:31 +0300

    string

--- src/services/auth.js
+++ src/services/auth.js
@@ -32,7 +32,7 @@ export const setSessionCookies = (res, session) => {
     maxAge: ONE_DAY,
   });
 
-  res.cookie('sessionId', session._id.toString(), {
+  res.cookie('sessionId', session._id, {
     httpOnly: true,
     secure: true,
     sameSite: 'none',
